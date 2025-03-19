9a4c68170878814aaf4f3d40e54a0e37bdc5e087
Tester Present Specialist Automotive Solutions <jakka351@outlook.com>
2025-03-19 14:02:04 +1100

    Update ABS_CAN_Interface.ino

--- ABS_CAN_Interface.ino
+++ ABS_CAN_Interface.ino
@@ -1,22 +1,91 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                             
+//                             ___________              __                 __________                                      __                         
+//                             \__    ___/___   _______/  |_  ___________  \______   \_______   ____   ______ ____   _____/  |_                       
+//                               |    |_/ __ \ /  ___/\   __\/ __ \_  __ \  |     ___/\_  __ \_/ __ \ /  ___// __ \ /    \   __\                      
+//                               |    |\  ___/ \___ \  |  | \  ___/|  | \/  |    |     |  | \/\  ___/ \___ \\  ___/|   |  \  |                        
+//                               |____| \___  >____  > |__|  \___  >__|     |____|     |__|    \___  >____  >\___  >___|  /__|                        
+//                                          \/     \/            \/                                \/     \/     \/     \/                            
+//                               _________                    .__       .__  .__          __                                                          
+//                              /   _____/_____   ____   ____ |__|____  |  | |__| _______/  |_                                                        
+//                              \_____  \\____ \_/ __ \_/ ___\|  \__  \ |  | |  |/  ___/\   __\                                                       
+//                              /        \  |_> >  ___/\  \___|  |/ __ \|  |_|  |\___ \  |  |                                                         
+//                             /_______  /   __/ \___  >\___  >__(____  /____/__/____  > |__|                                                         
+//                                     \/|__|        \/     \/        \/             \/                                                               
+//                                _____          __                         __  .__                                                                   
+//                               /  _  \  __ ___/  |_  ____   _____   _____/  |_|__|__  __ ____                                                       
+//                              /  /_\  \|  |  \   __\/  _ \ /     \ /  _ \   __\  \  \/ // __ \                                                      
+//                             /    |    \  |  /|  | (  <_> )  Y Y  (  <_> )  | |  |\   /\  ___/                                                      
+//                             \____|__  /____/ |__|  \____/|__|_|  /\____/|__| |__| \_/  \___  >                                                     
+//                                     \/                         \/                          \/                                                      
+//                               _________      .__          __  .__                                                                                  
+//                              /   _____/ ____ |  |  __ ___/  |_|__| ____   ____   ______                                                            
+//                              \_____  \ /  _ \|  | |  |  \   __\  |/  _ \ /    \ /  ___/                                                            
+//                              /        (  <_> )  |_|  |  /|  | |  (  <_> )   |  \\___ \                                                              
+//                             /_______  /\____/|____/____/ |__| |__|\____/|___|  /____  >                                                           
+//                                     \/                                       \/     \/                                                             
+//
+//
+//
+//                              Tester Present Specialist Automotive Solutions Open Source License (TPSAS-OSL)
+//                              Version 1.0, 2025
+//
+//                              Copyright (c) 2025 Tester Present Specialist Automotive Solutions
+//
+//                              Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+//                              documentation files (the "Software"), to use, modify, and distribute the Software subject to the following conditions:
+//
+//          1. Attribution.
+//            - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//            - Credit must be given to "Tester Present Specialist Automotive Solutions" in any documentation, public notices, or product materials 
+//              that acknowledge the use of the Software.//
+//
+//          2. Open Source Use Only.
+//            - The Software, including any modifications or derivative works (collectively, “Derivatives”), must be distributed only as open source software.
+//            - Derivatives or distributions of the Software must be licensed under this license or another license approved by the Open Source Initiative 
+//              (OSI) that maintains equivalent open source freedoms.
+//            - The Software and any Derivatives may not be used as a component in any closed source or proprietary software product.
+//
+//          3. Redistribution.
+//            - Redistributions of the Software, in source or binary form, must reproduce the above attribution, this license, and the disclaimer below 
+//              in the documentation and/or other materials provided with the distribution.
+//            - Any modifications made to the Software must be clearly marked as such, and the modified files must carry prominent notices stating that changes have been made.
+//
+//          4. No Warranty.
+//            - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//              FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT.
+//            - IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+//              TORT, OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//          5. Termination.
+//            - This license and the rights granted hereunder will terminate automatically upon any breach of the above conditions.
+//            - Upon termination, you must cease all use and distribution of the Software and any Derivatives.
+//
+//          6. Entire Agreement.
+//            - This license constitutes the entire agreement between you and Tester Present Specialist Automotive Solutions regarding the Software 
+//              and supersedes any prior understandings or agreements, whether written or oral, regarding the subject matter herein.
+//
+//          By using, modifying, or distributing the Software, you agree to abide by all the terms and conditions of this license.
+//
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                             
 //Isolated ABS Module CAN Interface
 //For a conversion where an XF Falcon is fitted with complete running gear from a mark 1 FG Falcon, running the 5.0 Miami Engine which has no matching ABS calibration in the older ABS module. 
 //To stop the error on the dash and get the ABS module to function, we are Man in the Middle attacking the CANbus, by isolating the ABS Module's CAN and intercepting all traffic sent to and from
 // the Module, editing the data such that the ABS module does not go into fault mode and cause a warning light to appear on the instrumnent cluster. Wheel Speed Sensor and other data such as Steering
 // Angle and Yaw Rate pass through the interface unchanged for use by the PCM. We are simulating a 5.4 litre engine configuration to the ABS module such that it will function normally.//
-
 //Problem
 //There is no matching ABS configuration for the very last of the mark 1 FG Falcons fitted with the 5.0 Miami Engine, in the context of a conversion where an older ABS module is used, the result 
 //is a C1991-60 Fault Code in the ABS (Module Calibration Failure) and the ABS subsequently goes into fault mode.//
-
 //Solution
 //Trick the ABS module into working by telling it is working with a Boss 5.4 Litre engine, by intercepting and editing the CAN traffic sent to the ABS Module.
+//////////////////////////////////////////////////////
 #include <Wire.h>
 #include <stdio.h>
 #include "canbed_dual.h"
-
+//////////////////////////////////////////////////////
 CANBedDual PCMCAN(0);
 CANBedDual ABSCAN(1);
-
+//////////////////////////////////////////////////////
 void setup()
 {
     Serial.begin(115200);
@@ -28,7 +97,7 @@ void setup()
     PCMCAN.init(500000);          // CAN0 baudrate: 500kb/s
     ABSCAN.init(500000);          // CAN1 baudrate: 500kb/s
 }
-
+//////////////////////////////////////////////////////
 void loop()
 {
     unsigned long id = 0;
@@ -111,6 +180,7 @@ void loop()
 
     }
 }
+//////////////////////////////////////////////////////
 const uint32_t retransmitOntoPcmBusIDs[] = {  // IDS THAT ARE RETRANSMITTED ONTO THE PCM BUS
   0x70,   // YAW_MSG_1
   0x75,   // YAW_MSG_2
@@ -143,10 +213,12 @@ const uint32_t editPCM[] = {  //INTEREPT IDS PCMCAN
 const uint32_t editABS[] = {  // INTERCEPT IDS ABSCAN
   0x210    // PCM_MSG_1
 };
+//////////////////////////////////////////////////////
 const int PCMIDs = sizeof(retransmitOntoPcmBusIDs) / sizeof(retransmitOntoPcmBusIDs[0]);
 const int ABSIDs = sizeof(retransmitOntoAbsBusIDs) / sizeof(retransmitOntoAbsBusIDs[0]);
 const int numPCMIDs = sizeof(editPCM) / sizeof(editPCM[0]);
 const int numABSIDs = sizeof(editABS) / sizeof(editABS[0]);
+//////////////////////////////////////////////////////
 // Check if a CAN ID should be edited for which bus...
 bool isEditPCM(uint32_t id) {
   for (int i = 0; i < numPCMIDs; i++) {
@@ -176,17 +248,9 @@ bool retransmitAbs(uint32_t id) {
   }
   return false;
 }
-// Edit the data for specific CAN IDs with example modifications.
-// The method first decodes the incoming 8-byte message into its individual fields
-// (as defined in the DBC), optionally modifies some parameters, and then reassembles
-// the message back into the 8-byte array.
-// 
-// Supported CAN IDs (in hexadecimal):
-//   0x210: ABS_MSG_1
-//   0x623: PCM_MSG_11
-//   0x640: PCM_MSG_12
-//   0x650: PCM_MSG_13 (m16 group used in this example)
+//////////////////////////////////////////////////////
 int alter650 = 0;
+//////////////////////////////////////////////////////
 void editData(uint32_t id, unsigned char *data, unsigned char len) {
   if (len < 8) return;  // Ensure we have 8 bytes
   // Use a 64-bit container to simplify bit operations.
